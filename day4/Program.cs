60d383aba00b55ae8c9fbef3605d9317250bbae9
Matt Sharpe <mds@live.co.uk>
2022-12-04 09:12:14 +0000

    d4p1

---
+++ day4/Program.cs
@@ -0,0 +1,25 @@
+﻿var input = await System.IO.File.ReadAllLinesAsync(args[0]);
+
+int[] ParseRange(string str)
+{
+    var start = int.Parse(str.Split('-')[0]);
+    var end = int.Parse(str.Split('-')[1]);
+    return Enumerable.Range(start, end - (start - 1)).ToArray();
+}
+
+bool GetFullyContains(IEnumerable<int> a, IEnumerable<int> b)
+    => a.All(b.Contains) || b.All(a.Contains);
+
+int countFullyContains = 0;
+
+foreach (var pairString in input)
+{
+    var ranges = pairString.Split(',').Select(ParseRange).ToArray();
+
+    if (GetFullyContains(ranges[0], ranges[1]))
+    {
+        countFullyContains++;
+    }
+}
+
+Console.WriteLine(countFullyContains.ToString());
