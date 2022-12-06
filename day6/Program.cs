37c8a951c28221eb1c48a37f19f6e0db78f06de9
Matthew Sharpe <matthew.sharpe@enable.com>
2022-12-06 12:26:55 +0000

    day6

---
+++ day6/Program.cs
@@ -0,0 +1,11 @@
+﻿var input = await System.IO.File.ReadAllTextAsync(args[0]);
+var markerLength = int.Parse(args[1]);
+
+for (var i = markerLength - 1; i < input.Length; i++)
+{
+    if (input.Substring(i - (markerLength - 1), markerLength).Distinct().Count() == markerLength)
+    {
+        Console.WriteLine($"Marker found after {i + 1} characters arrived.");
+        break;
+    }
+}
