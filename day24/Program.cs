ff846aa7e7c01aa040bc6930633391f312522606
Matt Sharpe <mds@live.co.uk>
2022-12-24 20:07:41 +0000

    d24p1 wip

--- day24/Program.cs
+++ day24/Program.cs
@@ -1,42 +1,5 @@
 ﻿var input = await System.IO.File.ReadAllLinesAsync(args[0]);
-var valley = new Valley(input.Max(o => o.Length), input.Length);
-
-for (var y = 0; y < valley.Height; y++)
-{
-    for (var x = 0; x < valley.Width; x++)
-    {
-        var c = input[y][x];
-        var loc = new Location();
-
-        switch (c)
-        {
-            case '#':
-                loc.IsWall = true;
-                break;
-
-            case '^':
-            case 'v':
-            case '<':
-            case '>':
-                valley.Occupants.Add(new Blizzard
-                {
-                    Coordinate = new Coordinate
-                    {
-                        X = x,
-                        Y = y
-                    },
-                    Direction = Direction.Parse(c)
-                });
-
-                break;
-
-            default:
-                break;
-        }
-
-        valley.Map[x, y] = loc;
-    }
-}
+var valley = new Valley(input);
 
 ConsoleMap.WriteMap(valley);
 await Task.Delay(1000);
--- day24/Valley.cs
+++ day24/Valley.cs
@@ -5,8 +5,48 @@ internal class Valley
     public int Height => Map.GetLength(1);
     public List<Actor> Occupants { get; } = new List<Actor>();
 
-    public Valley(int width, int height)
+    public Valley(string[] input)
     {
+        var width = input.Max(o => o.Length);
+        var height = input.Length;
+
         Map = new Location[width, height];
+
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                var c = input[y][x];
+                var loc = new Location();
+
+                switch (c)
+                {
+                    case '#':
+                        loc.IsWall = true;
+                        break;
+
+                    case '^':
+                    case 'v':
+                    case '<':
+                    case '>':
+                        Occupants.Add(new Blizzard
+                        {
+                            Coordinate = new Coordinate
+                            {
+                                X = x,
+                                Y = y
+                            },
+                            Direction = Direction.Parse(c)
+                        });
+
+                        break;
+
+                    default:
+                        break;
+                }
+
+                Map[x, y] = loc;
+            }
+        }
     }
 }
