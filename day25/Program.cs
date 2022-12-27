39094715aea2b00ac001640be011125b0a311cb4
Matt Sharpe <mds@live.co.uk>
2022-12-27 20:34:19 +0000

    d25p1 wip

--- day25/Program.cs
+++ day25/Program.cs
@@ -4,9 +4,18 @@ var sum = 0L;
 
 foreach (var snafu in input)
 {
+    Console.Write(snafu);
     var dec = SnafuConvert.ToInt64(snafu);
+    Console.Write($" > {dec}");
     var backToSnafu = SnafuConvert.ToSnafuString(dec);
-    Console.WriteLine($"{snafu} > {dec} > {backToSnafu}");
+    Console.WriteLine($" > {backToSnafu}");
+
+    if (backToSnafu != snafu)
+    {
+        throw new InvalidOperationException($"Snafu sanity check failed; '{backToSnafu}' != '{snafu}'");
+    }
+
+    sum += dec;
 }
 
 Console.WriteLine();
